775c941948904a04ead09dab80a990f74a6c3025
Pham Tuan <tuan.pham.super@gmail.com>
2025-07-18 18:16:56 +0700

    section 34 - implement logger with discord server

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const apiKey = require("../middlewares/apiKeyMiddleware");
 const permission = require("../middlewares/permissionMiddleware");
+const { pushToDiscordLog } = require("../middlewares/loggerMiddleware");
 
 const router = express.Router();
 
@@ -12,6 +13,9 @@ router.get("/", (req, res, next) => {
     message: "Welcome to Backend NodeJS Architecture!",
   });
 });
+
+// add log to discord
+router.use(pushToDiscordLog);
 
 // check api key
 router.use(apiKey);
